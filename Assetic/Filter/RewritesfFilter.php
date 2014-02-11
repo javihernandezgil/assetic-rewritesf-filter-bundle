ae4c65cb89b0c71a5186427838dc844d3b825e64
Javier Hernández Gil <javibilboweb@gmail.com>
2014-02-11 01:17:14 +0100

    Removes symfony/assetic code

--- Assetic/Filter/RewritesfFilter.php
+++ Assetic/Filter/RewritesfFilter.php
@@ -19,49 +19,8 @@ class RewritesfFilter implements FilterInterface
     public function filterDump(AssetInterface $asset)
     {
         $content = $asset->getContent();
-
-        $sourceBase = $asset->getSourceRoot();
-        $sourcePath = $asset->getSourcePath();
-        $targetPath = $asset->getTargetPath();
-        
-        if (null === $sourcePath || null === $targetPath || $sourcePath == $targetPath) {
-        	return;
-        }
-        
-        // learn how to get from the target back to the source
-        if (false !== strpos($sourceBase, '://')) {
-        	list($scheme, $url) = explode('://', $sourceBase.'/'.$sourcePath, 2);
-        	list($host, $path) = explode('/', $url, 2);
-        
-        	$host = $scheme.'://'.$host.'/';
-        	$path = false === strpos($path, '/') ? '' : dirname($path);
-        	$path .= '/';
-        } else {
-        	// assume source and target are on the same host
-        	$host = '';
-        
-        	// pop entries off the target until it fits in the source
-        	if ('.' == dirname($sourcePath)) {
-        		$path = str_repeat('../', substr_count($targetPath, '/'));
-        	} elseif ('.' == $targetDir = dirname($targetPath)) {
-        		$path = dirname($sourcePath).'/';
-        	} else {
-        		$path = '';
-        		while (0 !== strpos($sourcePath, $targetDir)) {
-        			if (false !== $pos = strrpos($targetDir, '/')) {
-        				$targetDir = substr($targetDir, 0, $pos);
-        				$path .= '../';
-        			} else {
-        				$targetDir = '';
-        				$path .= '../';
-        				break;
-        			}
-        		}
-        		$path .= ltrim(substr(dirname($sourcePath).'/', strlen($targetDir)), '/');
-        	}
-        }
 		   
-        $content = $this->filterReferences($asset->getContent(), function($matches) use ($host, $path) {
+        $content = $this->filterReferences($asset->getContent(), function($matches) {
         	
         	global $kernel;
         	$resource = $kernel->locateResource($matches[1]);
